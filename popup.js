a09d14cb16b9e4e5210898582358ccddde69e3e8
thiagoc-machado <thiagocmachado@yahoo.com.br>
2022-12-14 17:20:36 +0100

    popup

---
+++ popup.js
@@ -0,0 +1,60 @@
+
+//Porject 1
+var btnAbrirPopup = document.getElementById('proj1-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+
+//Porject 2
+var btnAbrirPopup = document.getElementById('proj2-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+
+//Porject 3
+var btnAbrirPopup = document.getElementById('proj3-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+
+//Porject 4
+var btnAbrirPopup = document.getElementById('4-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();   
-  });
+
 //contat
 const btn = document.getElementById('button');
 
@@ -46,79 +44,6 @@ function responsiveMenu() {
         x.className = "";
     }
 }
-
-
-
-
-
-
-
-
-    // // The item (or items) to press and hold on
-    // let item = document.querySelector("#item");
-
-    // let timerID;
-    // let counter = 0;
-
-    // let pressHoldEvent = new CustomEvent("pressHold");
-
-    // // Increase or decreae value to adjust how long
-    // // one should keep pressing down before the pressHold
-    // // event fires
-    // let pressHoldDuration = 50;
-
-    // // Listening for the mouse and touch events    
-    // item.addEventListener("mousedown", pressingDown, false);
-    // item.addEventListener("mouseup", notPressingDown, false);
-    // item.addEventListener("mouseleave", notPressingDown, false);
-
-    // item.addEventListener("touchstart", pressingDown, false);
-    // item.addEventListener("touchend", notPressingDown, false);
-
-    // // Listening for our custom pressHold event
-    // item.addEventListener("pressHold", doSomething, false);
-
-    // function pressingDown(e) {
-    //   // Start the timer
-    //   requestAnimationFrame(timer);
-
-    //   e.preventDefault();
-
-    //   console.log("Pressing!");
-    // }
-
-    // function notPressingDown(e) {
-    //   // Stop the timer
-    //   cancelAnimationFrame(timerID);
-    //   counter = 0;
-
-    //   console.log("Not pressing!");
-    // }
-
-    // //
-    // // Runs at 60fps when you are pressing down
-    // //
-    // function timer() {
-    //   console.log("Timer tick!");
-
-    //   if (counter < pressHoldDuration) {
-    //     timerID = requestAnimationFrame(timer);
-    //     counter++;
-    //   } else {
-    //     console.log("Press threshold reached!");
-    //     item.dispatchEvent(pressHoldEvent);
-    //   }
-    // }
-
-    // function doSomething(e) {
-    //   console.log("pressHold event fired!");
-    // }
-
-
-
-
-
-
 
 // Extend the base functionality of JavaScript
 Array.prototype.last = function () {
@@ -299,10 +224,6 @@ Array.prototype.last = function () {
         window.requestAnimationFrame(animate);
     }
     });
-
-
-
-
   window.addEventListener("mouseup", function (event) {
     if (phase == "stretching") {
       phase = "turning";
