287c9687a384628eff8dd533e0cb0df8f5442575
thiagoc-machado <thiagocmachado@yahoo.com.br>
2022-12-14 20:47:49 +0100

    popup

--- popup.js
+++ popup.js
@@ -14,44 +14,15 @@ btnCerrarPopup.addEventListener('click', function(e){
 	popup.classList.remove('active');
 });
 
+
 //Porject 2
 var btnAbrirPopup = document.getElementById('proj2-popup'),
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
+	overlay2 = document.getElementById('overlay2'),
+	popup2 = document.getElementById('popup2'),
 	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 btnAbrirPopup.addEventListener('click', function(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
-});
-btnCerrarPopup.addEventListener('click', function(e){
-	e.preventDefault();
-	overlay.classList.remove('active');
-	popup.classList.remove('active');
-});
-
-//Porject 3
-var btnAbrirPopup = document.getElementById('proj3-popup'),
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
-btnAbrirPopup.addEventListener('click', function(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
-});
-btnCerrarPopup.addEventListener('click', function(e){
-	e.preventDefault();
-	overlay.classList.remove('active');
-	popup.classList.remove('active');
-});
-
-//Porject 4
-var btnAbrirPopup = document.getElementById('4-popup'),
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
-btnAbrirPopup.addEventListener('click', function(){
-	overlay.classList.add('active');
-	popup.classList.add('active');
+	overlay2.classList.add('active');
+	popup2.classList.add('active');
 });
 btnCerrarPopup.addEventListener('click', function(e){
 	e.preventDefault();
