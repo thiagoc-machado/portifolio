a0345e9f08f6aa9ec43fb8f23f9d9f0b42edb1ef
thiagoc-machado <thiagocmachado@yahoo.com.br>
2022-12-16 12:22:41 +0100

    responsive menu

--- script.js
+++ script.js
@@ -19,20 +19,18 @@ document.getElementById('form')
     });
 });
 
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
 function seleccionar(link) {
-    var opciones = document.querySelectorAll('#links  a');
-    opciones[0].className = "";
-    opciones[1].className = "";
-    opciones[2].className = "";
-    opciones[3].className = "";
-    opciones[4].className = "";
-    link.className = "seleccionado";
+  var opciones = document.querySelectorAll('#links  a');
+  opciones[0].className = "";
+  opciones[1].className = "";
+  opciones[2].className = "";
+  opciones[3].className = "";
+  link.className = "seleccionado";
 
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
-    //en modo responsive
-    var x = document.getElementById("nav");
-    x.className = "";
+  //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+  //en modo responsive
+  var x = document.getElementById("nav");
+  x.className = "";
 }
 
 //función que muestra el menu responsive
