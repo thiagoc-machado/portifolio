c59b33316700e027afb078e11dc1d3d608d0b8c9
thiagoc-machado <thiagocmachado@yahoo.com.br>
2022-12-20 10:48:23 +0100

    flags

--- Scrips/popup.js
+++ Scrips/popup.js
@@ -5,22 +5,26 @@ var btnAbrirPopup = document.getElementById('proj1-popup'),
     btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 
 btnAbrirPopup.addEventListener('click', function() {
+  document.body.style.overflow = "hidden";
   overlay.classList.add('active');
   popup.classList.add('active');
 });
 
 btnCerrarPopup.addEventListener('click', function(e) {
   e.preventDefault();
+  document.body.style.overflow = "auto";
   overlay.classList.remove('active');
   popup.classList.remove('active');
 });
 
 overlay.addEventListener('click', function(event) {
   if (event.target === overlay) {
+    document.body.style.overflow = "auto";
     overlay.classList.remove('active');
     popup.classList.remove('active');
   }
 });
+
 
 // Project 2
 var btnAbrirPopup2 = document.getElementById('proj2-popup'),
@@ -29,17 +33,20 @@ var btnAbrirPopup2 = document.getElementById('proj2-popup'),
     btnCerrarPopup2 = document.getElementById('btn-cerrar-popup2');
 
 btnAbrirPopup2.addEventListener('click', function() {
+  document.body.style.overflow = "hidden";
   overlay2.classList.add('active');
   popup2.classList.add('active');
 });
 
 btnCerrarPopup2.addEventListener('click', function(e) {
+  document.body.style.overflow = "auto";
   e.preventDefault();
   overlay2.classList.remove('active');
   popup2.classList.remove('active');
 });
 
 overlay2.addEventListener('click', function(event) {
+  document.body.style.overflow = "auto";
   if (event.target === overlay2) {
     overlay2.classList.remove('active');
     popup2.classList.remove('active');
@@ -53,11 +60,14 @@ var btnAbrirPopup3 = document.getElementById('proj3-popup'),
     btnCerrarPopup3 = document.getElementById('btn-cerrar-popup3');
 
 btnAbrirPopup3.addEventListener('click', function() {
+  document.body.style.overflow = "auto";
+  document.body.style.overflow = "hidden";
   overlay3.classList.add('active');
   popup3.classList.add('active');
 });
 
 btnCerrarPopup3.addEventListener('click', function(e) {
+  document.body.style.overflow = "auto";
   e.preventDefault();
   overlay3.classList.remove('active');
   popup3.classList.remove('active');
@@ -77,17 +87,20 @@ var btnAbrirPopup4 = document.getElementById('proj4-popup'),
     btnCerrarPopup4 = document.getElementById('btn-cerrar-popup4');
 
 btnAbrirPopup4.addEventListener('click', function() {
+  document.body.style.overflow = "hidden";
   overlay4.classList.add('active');
   popup4.classList.add('active');
 });
 
 btnCerrarPopup4.addEventListener('click', function(e) {
+  document.body.style.overflow = "auto";
   e.preventDefault();
   overlay4.classList.remove('active');
   popup4.classList.remove('active');
 });
 
 overlay4.addEventListener('click', function(event) {
+  document.body.style.overflow = "auto";
   if (event.target === overlay4) {
     overlay4.classList.remove('active');
     popup4.classList.remove('active');
